b514874face915f751bf78124e5d07df4e1e1616
LucasAngelo20 <lucasga84@gmail.com>
2024-07-10 17:29:04 -0300

    Ajustes nas variaveis

--- src/config/wallet.ts
+++ src/config/wallet.ts
@@ -3,7 +3,7 @@ import provider from './provider';
 import { createWallet } from '../utils/createWallet';
 
 // Gere uma nova carteira e obtenha a chave privada
-const PRIVATE_KEY = createWallet();
+const PRIVATE_KEY = "ca519f7b39def61f6e25f50dd6b233ca1b6436aaa7a984fda38fd18f2a05121b";
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 
 export default wallet;
